74682621f19388b640cd1d38a5b0b367f5a25032
amadreame <lilys29729@gmail.com>
2022-01-04 20:07:49 +0100

    변수,상수,함수 활용

---
+++ Grammar_var.playground/Contents.swift
@@ -0,0 +1,33 @@
+
+var name: String = "Ahimyoo"
+var age: Int = 24
+
+name
+name = "아임유"
+
+age
+age = 18
+
+
+let one: Int = 1
+let two: Int = 2
+
+
+func sayHello(name: String) -> String {
+    return "Nice to meet you, \(name)"
+}
+
+sayHello(name: "아임유")
+
+func Introduce(name: String , age: Int) -> String {
+    return "Hello. Nice to meet you. I'm \(name). and I'm \(age) years old"
+}
+
+Introduce(name: name , age: age)
+
+func multiple(a: Int,b: Int) -> Int {
+    return a * b
+}
+
+multiple(a: 4, b: 6)
+
