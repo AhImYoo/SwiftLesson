864af5f330896e586635bab54269be1258869d8e
amadreame <lilys29729@gmail.com>
2022-01-05 02:28:43 +0100

    DataType 실습

---
+++ DataType.playground/Contents.swift
@@ -0,0 +1,35 @@
+
+var isChecked: Bool = false
+isChecked = true
+
+var temperature: Int = -100
+temperature = 100
+
+var cakes: UInt = 100
+//cakes = -1
+
+var pi: Float = 3.14
+pi = 44
+
+var pi2: Double = 3.14
+pi2 = 315
+
+var sampleCharacter: Character = "A"
+sampleCharacter = "b"
+
+var sampleString: String = "HI"
+sampleString = "Nice"
+
+var test = 100
+type(of:test)
+
+var sampleAny: Any = "any"
+sampleAny = 12
+sampleAny = 2.14
+
+var sampleInt: Int? = nil
+type(of: sampleInt)
+
+var sampleStr: String? = nil
+type(of: sampleStr)
+
